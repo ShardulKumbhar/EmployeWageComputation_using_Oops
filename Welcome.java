b7bf5641a8e29be98e9cdf529cf192b22249a444
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-02-21 20:03:50 +0530

    added welcome file

---
+++ Welcome.java
@@ -0,0 +1,8 @@
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation");
+		System.out.println("Using Oops Concept");
+	}
+}
