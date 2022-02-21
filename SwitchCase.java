185a7559dcfb5e8ed5478a53867222aee6d1939d
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-02-21 20:15:32 +0530

    added uc4 problem

---
+++ SwitchCase.java
@@ -0,0 +1,39 @@
+class employee4 {
+	void partTime() {
+		// variable
+		int empRatePerHr = 20;
+		int empHrs = 0;
+		double salary = 0;
+
+		double randomCheck = Math.floor(Math.random() * 10) % 3;
+		int i = (int) randomCheck;
+
+		// switch case
+		switch (i) {
+
+		case 2:
+			System.out.println("Employee is FullTime");
+			empHrs = 8;
+			break;
+		case 1:
+			System.out.println("Employee is PartTime");
+			empHrs = 4;
+			break;
+		case 0:
+			System.out.println("Employee is Absent");
+			empHrs = 0;
+			break;
+		}
+		// print employee salary
+		salary = empHrs * empRatePerHr;
+		System.out.println("Employee Salary" + salary);
+	}
+}
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		employee4 r = new employee4();
+		r.partTime();
+	}
+}
